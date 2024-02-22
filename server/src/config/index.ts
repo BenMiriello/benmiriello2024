057cb6b1468d9296723a2a2e0cefe0ebc2aaa1d6
BenMiriello <benmiriello@gmail.com>
2024-02-22 15:59:40 -0800

    [server] update env/config
    remove start-dev
    
    Add back start-dev

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,30 +2,21 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const port = process.env.PORT || 8000;
+const environment = process.env.NODE_ENV || 'production';
+export const apiUrl = environment === 'development' ?
+  `http://localhost:${port}/api/v1` :
+  'https://productiondomain.com/api/v1';
+
 export const openaiApiKey = process.env.OPENAI_API_KEY;
-export const model = 'gpt-3.5-turbo';
-// const environment = process.env.NODE_ENV || 'production';
-// export const defaultAssistantId = 'asst_D7tibdXiwYIL7HTXMYscLHMZ';
-export const defaultAssistantId = 'asst_pDZFbJLj6PWCi2U2qkk2v8Qf';
+export const model = process.env.GPT_MODEL || 'gpt-3.5-turbo';
+export const defaultAssistantId = process.env.ASSISTANT_ID || 'asst_pDZFbJLj6PWCi2U2qkk2v8Qf';
 
-// const environmentConfigs = {
-//   development: {
-//     apiUrl: `http://localhost:${port}/api/v1`,
-//   },
-//   // production: {
-//   //   apiUrl: 'https://productiondomain.com/api/v1',
-//   // },
-// };
-// export const apiUrl =...environmentConfigs[environment];
-
-export const apiUrl = `http://localhost:${port}/api/v1`;
+export const rateLimit = {
+  dailyRequestsHardMax: process.env.DAILY_REQUESTS_HARD_MAX || 300,
+  dailyRequestsSoftMax: process.env.DAILY_REQUESTS_SOFT_MAX || 200,
+};
 
 export const featureFlags = {
   textingEnabled: process.env.FEATURE_TEXTING_ENABLED === 'true',
   emailEnabled: process.env.FEATURE_EMAIL_ENABLED === 'true',
-}
-
-export const rateLimit = {
-  dailyRequestsHardMax: process.env.DAILY_REQUESTS_HARD_MAX || 20,
-  dailyRequestsSoftMax: process.env.DAILY_REQUESTS_SOFT_MAX || 10,
 };
