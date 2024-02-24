5ab3b96eac054c92326233e6651ec88b2b9c8d65
BenMiriello <benmiriello@gmail.com>
2024-02-24 14:13:55 -0800

    [client] UI move chat under card, intro, mobile-friendly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import robotAvatar from'./assets/robot.png';
+import cardImage from './assets/cardImage.png';
 import LoadingEllipsis from './components/LoadingEllipsis';
 
 interface ThreadContent {
@@ -26,8 +27,8 @@ interface MessageResponse { threadId: string, messages: ThreadMessage[] };
 
 const geAssistantPrompt = () => {
   const placeholders = [
-    "Ask me anything...",
-    "Ask me your burning questions...",
+    "Ask me anything",
+    "Ask me your burning questions",
   ];
 
   return placeholders[Math.floor(Math.random() * placeholders.length)];
@@ -115,36 +116,38 @@ const App = () => {
     }
   };
 
-  const assistantAvatar = (role: string) => {
-    if (role === 'assistant') {
-      return <img src={robotAvatar} className='assistant-avatar' />
-    }
-  }
-
   return (
     <div className="app">
-      <div className="chat-window">
-        <div className="messages">
-          {messages.map((msg) => (
-            <div key={msg.id} id={msg.id} className={`message ${msg.role}`}>
-              { assistantAvatar(msg.role) }
-              {msg.content[0].text.value}
-            </div>
-          ))}
-          { waiting && <LoadingEllipsis />}
-          <div ref={messagesEndRef} />
+      <div className='main-content-container'>
+        <div className='header' style={{ backgroundImage: `url(${cardImage})` }}></div>
+        <div className='introduction'>
+          Hi, I'm Ben Miriello! I made this site so you can:
         </div>
-        <form onSubmit={handleSubmit} className="message-form">
-          <textarea
-            className="message-input"
-            value={inputValue}
-            onChange={(e) => { setInputValue(e.target.value); adjustTextareaHeight(e); }}
-            onKeyDown={handleKeyDown}
-            placeholder={'Message Miriello GPT...'}
-            rows={1}
-          ></textarea>
-          <button type="submit" className="send-button">Send</button>
-        </form>
+        <div className="chat-window">
+          <div className="messages">
+            {messages.map((msg) => (
+              <div key={msg.id} id={msg.id} className={`message ${msg.role}`}>
+                { msg.role !== 'assistant' ? null :
+                  <img src={robotAvatar} alt='face of a friendly robot assistant' className='assistant-avatar' />
+                }
+                {msg.content[0].text.value}
+              </div>
+            ))}
+            { waiting && <LoadingEllipsis />}
+            <div ref={messagesEndRef} />
+          </div>
+          <form onSubmit={handleSubmit} className="message-form">
+            <textarea
+              className="message-input"
+              value={inputValue}
+              onChange={(e) => { setInputValue(e.target.value); adjustTextareaHeight(e); }}
+              onKeyDown={handleKeyDown}
+              placeholder={'Message Miriello GPT...'}
+              rows={1}
+            ></textarea>
+            <button type="submit" className="send-button">Send</button>
+          </form>
+        </div>
       </div>
     </div>
   );
